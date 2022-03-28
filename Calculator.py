941f52cb5b76bf82aaa2490d141406944882762a
= <=>
2022-03-28 14:40:43 +0530

    Calculator.py added

---
+++ Calculator.py
@@ -0,0 +1,23 @@
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def multi(x,y):
+    return x*y
+
+def div(x,y):
+    return x/y
+
+if __name__ == "__main__":
+    num1 = float(input("ENTER NUM1: "))
+    num2 = float(input("ENTER NUM2: "))
+    result1 = add(num1, num2)
+    print(result1)
+    result2 = sub(num1, num2)
+    print(result2)
+    result3 = multi(num1, num2)
+    print(result3)
+    result4 = div(num1, num2)
+    print(result4)
